64929a67d6e7ec9da6f81f36e5bf79741fafdcb9
Carboncito <carboncito760@gmail.com>
2024-09-03 16:04:31 -0600

    upload env

--- src/app/utils/getPath.util.ts
+++ src/app/utils/getPath.util.ts
@@ -3,5 +3,6 @@ import { environment } from '../../environments/environment';
 export const getPath = () => {
   return environment.DEV
     ? `http://${document.location.hostname}:3000`
-    : 'http://localhost:3000';
+    : // ? `https://rem-backend-two.vercel.app`
+      'https://rem-backend-two.vercel.app';
 };
